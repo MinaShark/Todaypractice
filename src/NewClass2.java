713097924255d0341aa979b2b696e272209666d3
MinaShark <105183275+MinaShark@users.noreply.github.com>
2022-05-11 18:05:49 -0400

    change

--- src/NewClass2.java
+++ src/NewClass2.java
@@ -14,7 +14,8 @@ public class NewClass2 {
 		// i am happy it worked and hard work paid off
 		
 		// i have learend to work hard and be independent and never depend on someone
-		
+		// i have been always hard working and never was dependent on someone. The minute i depend on someone, I get a hard clap on my face. 
+		// so never depende on human beind and be dependent on your god
 	}
 
 }
